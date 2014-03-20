02a280140300f3678f4c6b5157054716e736b328
YAO PENG <vickypy0802@gmail.com>
2014-03-20 18:13:06 +0800

    doodle

---
+++ src/wahaha/graph.java
@@ -0,0 +1,10 @@
+package wahaha;
+
+import acm.graphics.*;
+import acm.program.*;
+
+public class graph extends GraphicsProgram {
+	public void run() {
+		add(new GLabel("我是天才挖哈哈", 250, 250));
+	}
+}
